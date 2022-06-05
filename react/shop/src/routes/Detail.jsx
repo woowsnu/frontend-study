4d9bae9b8c06f77ddcb3f2cdcc70b7aedd18cf2f
woowsnu <woowsnu@gmail.com>
2022-06-05 21:03:16 +0900

    feat: 게릴라 팝업 기능 추가(useEffect 실습)

--- react/shop/src/routes/Detail.jsx
+++ react/shop/src/routes/Detail.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 
 const Detail = (props) => {
+  useEffect(()=>{
+    setTimeout(()=>{ setAlert(false) }, 2000)
+  })
+
+  let [alert, setAlert] = useState(true)
   let {id} = useParams();
   let detailProduct = props.shell.find((x)=>{return x.id == id});
   return (
     <div className="container">
+      {alert && <div className='alert alert-warning'>
+        2초 이내 구매시 할인
+      </div>}      
     <div className="row">
       <div className="col-md-6">
         <h4 className="pt-5">{detailProduct.title}</h4>
