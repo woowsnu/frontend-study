5362a4685e76438dd06e34544da8c4e587c0ae90
woowsnu <woowsnu@gmail.com>
2022-06-24 23:25:40 +0900

    Udemy React 실습

---
+++ react/react-guide/first-section/src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import './ExpenseItem.css';
+
+//props는 모든 속성을 받는 '객체'
+const ExpenseItem = (props) => {
+    return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
