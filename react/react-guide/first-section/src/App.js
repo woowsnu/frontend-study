184aafb6c6a4d69bb8cb382b26b8f3cb103cb343
woowsnu <woowsnu@gmail.com>
2022-06-27 22:41:24 +0900

    Feat: Expense 영역 추가

--- react/react-guide/first-section/src/App.js
+++ react/react-guide/first-section/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -25,16 +25,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
+      <Expenses expenses={expenses}/>
     </div>
   );
 }
--- react/react-guide/first-section/src/components/ExpenseItem.js
+++ react/react-guide/first-section/src/components/ExpenseItem.js
@@ -1,10 +1,11 @@
-import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
 //props는 모든 속성을 받는 '객체'
 const ExpenseItem = (props) => {
-    return (
+  return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
