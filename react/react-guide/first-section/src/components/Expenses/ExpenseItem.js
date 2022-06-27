97a66ea05d0b9f5826beef9b3de01502d2722970
woowsnu <woowsnu@gmail.com>
2022-06-28 00:49:48 +0900

    Feat: Expense 입력 폼 추가

--- react/react-guide/first-section/src/components/Expenses/ExpenseItem.js
+++ react/react-guide/first-section/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 //props는 모든 속성을 받는 '객체'
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('updated!');
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <div onClick={clickHandler}>Change Title</div>
     </Card>
   );
 };
