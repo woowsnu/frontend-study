8078879c3408bd1bbbd880c1f8dbc0340ca26600
woowsnu <woowsnu@gmail.com>
2022-07-01 01:05:11 +0900

    Fear: SelectBox Filter 적용, Form 오픈/클로즈 적용

--- react/react-guide/first-section/src/components/Expenses/Expenses.js
+++ react/react-guide/first-section/src/components/Expenses/Expenses.js
@@ -11,6 +11,23 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No expense found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -18,12 +35,19 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.expenses.map((expense) => (<ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />)
-        )}
+        {expensesContent}
+        {/* {filteredExpenses.length === 0 ? (
+          <p>No expense found.</p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )} */}
       </Card>
     </div>
   );
