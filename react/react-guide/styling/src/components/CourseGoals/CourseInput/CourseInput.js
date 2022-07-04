de32f612a59de7115da7e2cffd347603391474f7
woowsnu <woowsnu@gmail.com>
2022-07-04 17:50:46 +0900

    Remove: 스타일링 파일 오류 파일 수정

--- react/react-guide/styling/src/components/CourseGoals/CourseInput/CourseInput.js
+++ react/react-guide/styling/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
     if (event.target.valuetrim().length > 0) {
       setIsValid(true);
     }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
     if (enteredValue.trim().length === 0) {
       setIsValid(false);
@@ -25,9 +25,9 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
-        <input style={{backgroundColor: !isValid ? 'red' : 'black', background: !isValid ? 'salmon' : 'transparent'}}type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${!isValid && 'invalid'}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
