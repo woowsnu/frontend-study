733c7eb52a9478073bd77f2bf693933f7defa219
woowsnu <woowsnu@gmail.com>
2022-07-09 23:40:12 +0900

    Feat: styles 파일 지정

---
+++ react/shop/src/styles/colors.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    black: "#000000",
+    white: "#FFFFFF",
+    gray1: "#333333",
+    gray2: "#4C4C4C",
+    gray3: "#999999",
+    gray4: "#F7F7F7",
+    primary1: "#5F0080",
+    primary2: "#BD76FF",
+    secondary1: "#FA622F",
+  };
+  
+  
+  
