c26e238385f58e2e41b6e397d4a949073358444b
woowsnu <woowsnu@gmail.com>
2022-08-11 02:45:19 +0900

    study how to use localstorage

--- Javascript/menu-app/src/js/index.js
+++ Javascript/menu-app/src/js/index.js
@@ -1,7 +1,52 @@
-
 const $ = (selector) => document.querySelector(selector);
 
+const store = {
+  setLocalstorage(menu) {
+    localStorage.setItem("menu", JSON.stringify(menu));
+  },
+  getLocalStorage() {
+    return JSON.parse(localStorage.getItem("menu"));
+  },
+};
+
 function App() {
+  // 상태 - 갯수, 메뉴명
+  this.menu = [];
+  this.init = () => {
+    if (store.getLocalStorage().length >= 1) {
+      this.menu = store.getLocalStorage();
+    }
+    render();
+  };
+
+  const render = () => {
+    const template = this.menu
+      .map((item, index) => {
+        return `
+      <li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
+      <span class="w-100 pl-2 menu-name">${item.name}</span>
+      <button
+        type="button"
+        class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+      >
+        수정
+      </button>
+      <button
+        type="button"
+        class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+      >
+        삭제
+      </button>
+    </li>
+    `;
+      })
+      .join("");
+
+    $("#espresso-menu-list").innerHTML = template;
+    editMenuCount();
+  }
+  ;
+
   const editMenuCount = () => {
     const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
     $(".menu-count").innerText = `총 ${menuCount} 개`;
@@ -13,42 +58,31 @@ function App() {
       return;
     }
     const espressoMenuName = $("#espresso-menu-name").value;
-    const menuItemTemplate = (espressoMenuName) => {
-      return `
-        <li class="menu-list-item d-flex items-center py-2">
-        <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
-        <button
-          type="button"
-          class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-        >
-          수정
-        </button>
-        <button
-          type="button"
-          class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-        >
-          삭제
-        </button>
-      </li>
-      `;
-    };
-    $("#espresso-menu-list").insertAdjacentHTML(
-      "beforeend",
-      menuItemTemplate(espressoMenuName)
-    );
-    editMenuCount();
+    this.menu.push({ name: espressoMenuName });
+    store.setLocalstorage(this.menu);
+    render();
     $("#espresso-menu-name").value = "";
   };
 
   const editMenuName = (e) => {
+    const menuId = e.target.closest("li").dataset.menuId;
     const $menuName = e.target.closest("li").querySelector(".menu-name");
-    const menuName = $menuName.innerText;
-    const editedMenuName = prompt("수정될 메뉴명을 입력하세요", menuName);
+    const editedMenuName = prompt(
+      "수정될 메뉴명을 입력하세요",
+      $menuName.innerText
+    );
+    this.menu[menuId].name = editedMenuName;
+    store.setLocalstorage(this.menu);
     $menuName.innerText = editedMenuName;
   };
 
   const removeMenuName = (e) => {
     if (confirm("메뉴를 삭제하시겠습니까?")) {
+      const menuId = e.target.closest("li").dataset.menuId;
+      // 로컬 스토리지의 배열에서 삭제하는 것!!
+      this.menu.splice(menuId, 1);
+      store.setLocalstorage(this.menu);
+      // 화면에 보이는 부분 삭제되는 것!
       e.target.closest("li").remove();
       editMenuCount();
     }
@@ -70,7 +104,6 @@ function App() {
   // 이벤트 객체를 따로 받지 않으면 click 이벤트 뒤에 함수 이름만 써줘서 주소값을 바인딩 해준다!
   $("#espresso-menu-submit-button").addEventListener("click", addMenuName);
 
-
   $("#espresso-menu-name").addEventListener("keypress", (e) => {
     if (e.key !== "Enter") {
       return;
@@ -79,4 +112,5 @@ function App() {
   });
 }
 
-App();
+const app = new App();
+app.init();
